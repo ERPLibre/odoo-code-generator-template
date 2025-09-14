89526129ec611c87bbc3698be5fa606e38b49958
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:33:29 -0400

    [MIG] code_generator_demo_website_snippet: Migration to 15.0

--- code_generator_demo_website_snippet/__manifest__.py
+++ code_generator_demo_website_snippet/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Code Generator Demo Website Snippet",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
     "website": "https://technolibre.ca",
