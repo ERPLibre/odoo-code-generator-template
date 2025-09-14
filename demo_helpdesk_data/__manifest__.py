1bb78586ea480ffcdc09c1b184f23a67658abfe6
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:34:33 -0400

    [MIG] demo_helpdesk_data: Migration to 16.0

--- demo_helpdesk_data/__manifest__.py
+++ demo_helpdesk_data/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Demo helpdesk data",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
     "website": "https://technolibre.ca",
