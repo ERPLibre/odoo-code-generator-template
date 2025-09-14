376fdc4c6555488360a95c695a9c059e236a8f4f
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:29:29 -0400

    [MIG] demo_website_snippet: Migration to 13.0

--- demo_website_snippet/__manifest__.py
+++ demo_website_snippet/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Demo Website Snippet",
     "category": "Website",
-    "version": "12.0.1.0",
+    "version": "13.0.1.0.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
     "website": "https://technolibre.ca",
