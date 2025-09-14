6e266f2b7a0bdce3563327da914f1148955b7ad9
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 01:55:27 -0400

    [MIG] theme_website_demo_code_generator: Migration to 17.0

--- theme_website_demo_code_generator/__manifest__.py
+++ theme_website_demo_code_generator/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Demo theme website",
     "category": "Theme",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
     "sequence": 900,
@@ -13,6 +13,10 @@
     "data": [
         "data/theme_website_demo_code_generator_data.xml",
         "views/theme_website_demo_code_generator_templates.xml",
+    ],
+    "website.assets_frontend": [
+        "theme_website_demo_code_generator/static/src/scss/_variables.scss",
+        "theme_website_demo_code_generator/static/src/scss/custom.scss",
     ],
     "installable": True,
 }
