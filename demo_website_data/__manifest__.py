5d484816375b93766e987cbbb4121e3ec9e314e5
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:34:32 -0400

    [MIG] demo_website_data: Migration to 16.0

--- demo_website_data/__manifest__.py
+++ demo_website_data/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Demo website data",
     "category": "Website",
     "summary": "Exported Data from website",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
     "website": "https://technolibre.ca",
