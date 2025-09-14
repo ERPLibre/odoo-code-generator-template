d05523a1e418388d2058857c81e34200fe0d2159
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 01:55:27 -0400

    [MIG] demo_website_attachments_data: Migration to 17.0

--- demo_website_attachments_data/hooks.py
+++ demo_website_attachments_data/hooks.py
@@ -7,31 +7,27 @@ from odoo import SUPERUSER_ID, _, api, fields, models
 _logger = logging.getLogger(__name__)
 
 
-def pre_init_hook(cr):
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        # Remove all website pages before installing data
+def pre_init_hook(env):
+    # Remove all website pages before installing data
 
-        website_page_ids = env["website.page"].search([])
-        website_menu_ids = env["website.menu"].search([])
-        # TODO website doesn't support multi
-        # website_page_ids.website_id = None
-        # TODO replace by :
-        for website_page in website_page_ids:
-            website_page.website_id = None
-        for website_menu in website_menu_ids:
-            website_menu.website_id = None
+    website_page_ids = env["website.page"].search([])
+    website_menu_ids = env["website.menu"].search([])
+    # TODO website doesn't support multi
+    # website_page_ids.website_id = None
+    # TODO replace by :
+    for website_page in website_page_ids:
+        website_page.website_id = None
+    for website_menu in website_menu_ids:
+        website_menu.website_id = None
 
 
-def post_init_hook(cr, e):
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        xml_id = "ir_attachment_/website/static/src/scss/options/colors/user_theme_color_palette_custom_web_assets_common_scss"
-        update_datas_ir_attachment_from_xmlid(env, xml_id)
-        xml_id = "ir_attachment_/website/static/src/scss/options/colors/user_color_palette_custom_web_assets_common_scss"
-        update_datas_ir_attachment_from_xmlid(env, xml_id)
-        xml_id = "ir_attachment_/web/static/src/scss/bootstrap_overridden_frontend_custom_web_assets_frontend_scss"
-        update_datas_ir_attachment_from_xmlid(env, xml_id)
+def post_init_hook(env):
+    xml_id = "ir_attachment_/website/static/src/scss/options/colors/user_theme_color_palette_custom_web_assets_common_scss"
+    update_datas_ir_attachment_from_xmlid(env, xml_id)
+    xml_id = "ir_attachment_/website/static/src/scss/options/colors/user_color_palette_custom_web_assets_common_scss"
+    update_datas_ir_attachment_from_xmlid(env, xml_id)
+    xml_id = "ir_attachment_/web/static/src/scss/bootstrap_overridden_frontend_custom_web_assets_frontend_scss"
+    update_datas_ir_attachment_from_xmlid(env, xml_id)
 
 
 def update_datas_ir_attachment_from_xmlid(env, xml_id):
