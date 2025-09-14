1076bc607fe42aa35f6b4a351cf93ed9d72e7f7a
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:32:14 -0400

    [MIG] code_generator_demo_internal: Migration to 14.0

--- code_generator_demo_internal/__manifest__.py
+++ code_generator_demo_internal/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Setup testing environment for internal view code generator",
     "category": "Uncategorized",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
     "depends": ["code_generator"],
