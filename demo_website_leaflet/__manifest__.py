9eaa73597695f71e7fe71f5f7992864867b5b375
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:32:14 -0400

    [MIG] demo_website_leaflet: Migration to 14.0

--- demo_website_leaflet/__manifest__.py
+++ demo_website_leaflet/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Demo website leaflet",
     "category": "Website",
     "summary": "Leaflet integration in website",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
     "website": "https://technolibre.ca",
