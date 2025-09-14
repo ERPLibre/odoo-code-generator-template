56c8f9f4241be8962b132d0ecb84beb295a7033e
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:34:32 -0400

    [MIG] business_plan_import_pdf: Migration to 16.0

--- business_plan_import_pdf/__manifest__.py
+++ business_plan_import_pdf/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Business Plan Import Pdf",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "TechnoLibre",
     "website": "https://technolibre.ca",
     "license": "AGPL-3",
