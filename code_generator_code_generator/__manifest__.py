2e0674c4997bac3d522355633d3a5678341d273d
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:29:29 -0400

    [MIG] code_generator_code_generator: Migration to 13.0

--- code_generator_code_generator/__manifest__.py
+++ code_generator_code_generator/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Code Generator Code Generator",
-    "version": "12.0.1.0",
+    "version": "13.0.1.0.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
     "website": "https://technolibre.ca",
