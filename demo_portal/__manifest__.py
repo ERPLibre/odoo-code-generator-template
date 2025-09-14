083561587e0bab10ca76f0b5b326fee756c07395
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:32:14 -0400

    [MIG] demo_portal: Migration to 14.0

--- demo_portal/__manifest__.py
+++ demo_portal/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Demo Portal",
     "category": "Uncategorized",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
     "website": "https://technolibre.ca",
