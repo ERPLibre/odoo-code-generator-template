df967f21fb3a68ecb2991a06fd5c946901d054ab
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:29:29 -0400

    [MIG] code_generator_template_demo_internal_inherit: Migration to 13.0

--- code_generator_template_demo_internal_inherit/__manifest__.py
+++ code_generator_template_demo_internal_inherit/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Code Generator Template Demo Internal Inherit",
-    "version": "12.0.1.0",
+    "version": "13.0.1.0.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
     "website": "https://technolibre.ca",
